5d7fab1e9dd90424d56d759c9a209ea652ac2418
Andre-eugenio-almeida <almeida.ae@outlook.com>
2024-03-12 15:22:15 -0300

    upgrade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from "@angular/core"; 
-import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent,  } from "./pages/home/home.component";
-import { MenuComponent } from "./pages/menu/menu.component";
-import { AboutComponent } from "./pages/about/about.component";
-import { ContactComponent } from "./pages/contact/contact.component";
+import { NgModule } from '@angular/core'; 
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 const routes: Routes = [
-    {path:'Home', component:HomeComponent},
+    {path:'', component:HomeComponent},
     {path:'menu', component:MenuComponent},
     {path:'about', component:AboutComponent},
     {path:'contact', component:ContactComponent},
