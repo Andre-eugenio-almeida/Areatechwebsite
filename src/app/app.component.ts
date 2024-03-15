01e2824b109206e3ba620e5407264fa16e5d2b33
Andre-eugenio-almeida <almeida.ae@outlook.com>
2024-03-15 20:50:06 -0300

    upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
 import { NavbarComponent } from "./sharepege/navbar/navbar.component";
 import { FooterComponent } from "./sharepege/footer/footer.component";
-import { HomeComponent } from "./pages/home/home.component";
+
 
 @Component({
     selector: 'app-root',
